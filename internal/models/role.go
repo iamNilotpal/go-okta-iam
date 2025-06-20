e345f5ad1993724c1b65312f1f484454540a9a86
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2025-06-20 16:04:09 +0530

    Add ConvertOktaIamRoleToModel and ConvertOktaRoleToModel methods

--- internal/models/role.go
+++ internal/models/role.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/okta/okta-sdk-golang/v5/okta"
+)
 
 const (
 	RoleTypeSystem string = "SYSTEM"
@@ -10,29 +14,24 @@ const (
 // Role represents a set of permissions that can be assigned to users or groups.
 // Examples: "Admin", "ReadOnly", "UserManager", "BillingViewer".
 type Role struct {
-	ID          string       `json:"id"`
-	Name        string       `json:"name"`
-	Description string       `json:"description"`
-	Type        string       `json:"type"`
-	Created     time.Time    `json:"created"`
-	LastUpdated time.Time    `json:"lastUpdated"`
-	Permissions []Permission `json:"permissions,omitempty"`
-	Users       []User       `json:"users,omitempty"`
-	Groups      []Group      `json:"groups,omitempty"`
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Type        string    `json:"type"`
+	Created     time.Time `json:"created"`
+	LastUpdated time.Time `json:"lastUpdated"`
 }
 
 // CreateRoleRequest represents the data needed to create a new role.
 type CreateRoleRequest struct {
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Permissions []string `json:"permissions,omitempty"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
 // UpdateRoleRequest represents the data that can be updated for a role.
 type UpdateRoleRequest struct {
-	Name        string   `json:"name,omitempty"`
-	Description string   `json:"description,omitempty"`
-	Permissions []string `json:"permissions,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
 }
 
 // UserRoleAssignment represents assigning a role to a user.
@@ -41,8 +40,26 @@ type UserRoleAssignment struct {
 	RoleID string `json:"roleId"`
 }
 
-// RolePermissionAssignment represents assigning a permission to a role.
-type RolePermissionAssignment struct {
-	RoleID       string `json:"roleId"`
-	PermissionID string `json:"permissionId"`
+func ConvertOktaIamRoleToModel(oktaRole *okta.IamRole) *Role {
+	role := &Role{
+		ID:          *oktaRole.Id,
+		Type:        RoleTypeCustom,
+		Name:        oktaRole.GetLabel(),
+		Description: oktaRole.GetDescription(),
+		Created:     oktaRole.GetCreated(),
+		LastUpdated: oktaRole.GetLastUpdated(),
+	}
+	return role
+}
+
+func ConvertOktaRoleToModel(assignment *okta.Role) *Role {
+	role := &Role{
+		ID:          assignment.GetId(),
+		Type:        RoleTypeCustom,
+		Name:        assignment.GetLabel(),
+		Description: assignment.GetDescription(),
+		Created:     assignment.GetCreated(),
+		LastUpdated: assignment.GetLastUpdated(),
+	}
+	return role
 }
